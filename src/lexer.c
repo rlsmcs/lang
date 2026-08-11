20c40f443760c269081e273e51cb0bc1bfcdb9ce
rlsmcs <213845870+rahulsathish07@users.noreply.github.com>
2026-08-11 16:30:40 +0000

    add number tokenization

--- src/lexer.c
+++ src/lexer.c
@@ -6,12 +6,20 @@ struct TokenVector lex(const char *source){
     struct TokenVector tokens;
     tokenVectorInit(&tokens); //init
     const char *current = source;
+    size_t line=1;
     while(*current !='\0'){
         if(isdigit(*current)){
             const char *start = current;
             while(isdigit(*current)){
                 current++;
             }
+            struct Token token;
+            token.type=TOKEN_NUMBER;
+            token.start=start;
+            token.length= (current-start);
+            token.line=line;
+            tokenVectorPush(&tokens,token);
+            continue;
         }
         current++;
     }
